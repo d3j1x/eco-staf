2620413bdf9caec87964ebe11e4748d71d354730
d3j1x <d3j1x@yahoo.com>
2025-09-16 21:42:24 +0100

    update features

--- src/lib/components/homeComponents/Features.svelte
+++ src/lib/components/homeComponents/Features.svelte
@@ -1,116 +1,34 @@
-<section class=" pb-6">
-    <div class="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
-        <div class="container mx-auto px-6 p-6 ">
+<script>
+    import fibreOptiqueLogo from '$lib/images/fibrelogo.jpg'
+</script>
 
 
-            <div class="mb-16 text-center">
-                <h4 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Fonctionnalités</h4>
-                <p class="mt-2 text-5xl lg:text-7xl font-bold tracking-tight ">Comment nous changeons les règles du jeu
-
-                </p>
-            </div>
+<section
+  class=" light relative py-[60px] md:pt-0 md:pb-[100px] z-[1] mx-auto my-10"
+>
 
 
-            <div class="flex flex-wrap my-12">
+  <div class="container px-16 md:px-8 lg:px-28 w-full m-auto">
+    <div class="grid grid-cols-12 justify-between">
+      <div class="col-span-12 lg:col-span-5 flex flex-col justify-center text-center lg:text-start">
+        <h2 class="font-bold text-[32px] leading-none md:text-[45px] mb-6 mt-0 md:mt-12">Bonne nouvelle !</h2>
+        <p class="text-lg opacity-80 mb-12 lg:mb-0">
+          La fibre optique représente aujourd’hui une avancée majeure dans le domaine des télécommunications.
+            C’est une technologie qui utilise des fils très fins en verre ou en plastique pour transmettre des données à la vitesse de la lumière.<br><br>
 
+Installer la fibre optique demande un savoir-faire technique et une formation spécialisée. 
+Les techniciens qui travaillent dans ce domaine exercent une véritable profession, car leur rôle ne se limite pas à un simple emploi : ils contribuent à connecter les foyers et les entreprises à un réseau moderne et performant.<br><br>
 
-                <div class="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8">
-                    <div class="flex items-center mb-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
-                            fill="currentColor" class="h-6 w-6 text-indigo-500">
-                            <path
-                                d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
-                            </path>
-                        </svg>
-                        <div class="ml-4 text-xl">Augmenter les ventes</div>
-                    </div>
-                    <p class="leading-loose ">Nos solutions vous aident à transformer plus de prospects en clients grâce à des outils de suivi performants et des stratégies adaptées.
-                    </p>
-                </div>
-
-
-
-                <div class="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8">
-                    <div class="flex items-center mb-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
-                            fill="currentColor" class="h-6 w-6 text-indigo-500">
-                            <path
-                                d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
-                            </path>
-                        </svg>
-                        <div class="ml-4 text-xl">Prêt pour l’entreprise</div>
-                    </div>
-                    <p class="leading-loose ">Une infrastructure robuste et sécurisée, conçue pour répondre aux besoins des petites comme des grandes entreprises.
-                    </p>
-                </div>
-
-
-
-                <div class="w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8">
-                    <div class="flex items-center mb-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
-                            fill="currentColor" class="h-6 w-6 text-indigo-500">
-                            <path
-                                d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
-                            </path>
-                        </svg>
-                        <div class="ml-4 text-xl">Croissance illimitée</div>
-                    </div>
-                    <p class="leading-loose ">Développez vos activités sans contrainte : notre plateforme évolue avec vous et s’adapte à votre rythme de croissance.
-                    </p>
-                </div>
-
-
-
-                <div class="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8">
-                    <div class="flex items-center mb-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
-                            fill="currentColor" class="h-6 w-6 text-indigo-500">
-                            <path
-                                d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
-                            </path>
-                        </svg>
-                        <div class="ml-4 text-xl">Recommandé par les experts</div>
-                    </div>
-                    <p class="leading-loose ">Des professionnels du secteur nous font confiance et recommandent nos solutions pour leur efficacité et leur fiabilité.
-                    </p>
-                </div>
-
-
-
-                <div class="w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8">
-                    <div class="flex items-center mb-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
-                            fill="currentColor" class="h-6 w-6 text-indigo-500">
-                            <path
-                                d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
-                            </path>
-                        </svg>
-                        <div class="ml-4 text-xl">Plateforme moderne</div>
-                    </div>
-                    <p class="leading-loose ">Une technologie de pointe, intuitive et rapide, pensée pour offrir une expérience utilisateur optimale.
-                    </p>
-                </div>
-
-
-
-                <div class="w-full md:w-1/2 lg:w-1/3 p-8">
-                    <div class="flex items-center mb-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
-                            fill="currentColor" class="h-6 w-6 text-indigo-500">
-                            <path
-                                d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
-                            </path>
-                        </svg>
-                        <div class="ml-4 text-xl">Intégrations</div>
-                    </div>
-                    <p class="leading-loose ">Connectez facilement nos services avec vos outils existants pour centraliser vos données et simplifier vos processus.
-                    </p>
-                </div>
-
-
-            
-            </div>
-        </div>
+La fibre optique est donc plus qu’un service : c’est une carrière d’avenir pour les professionnels qualifiés, et une révolution pour les utilisateurs qui bénéficient d’une connexion rapide, stable et fiable.
+        </p>
+      </div>
+      <div class="col-span-12 lg:col-span-5 lg:col-start-8 text-center">
+        <img
+          src={fibreOptiqueLogo}
+          alt=""
+          class="h-auto max-w-full mx-auto rounded-md"
+        />
+      </div>
     </div>
+  </div>
 </section>
