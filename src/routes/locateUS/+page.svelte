16be655780da1c50f6bf1d6d85b4255a650f8514
d3j1x <d3j1x@yahoo.com>
2025-09-18 17:54:30 +0100

    update app

--- src/routes/locateUS/+page.svelte
+++ src/routes/locateUS/+page.svelte
@@ -12,7 +12,7 @@
         <div class="mt-16 lg:mt-20">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div class="rounded-lg overflow-hidden">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4031883118428!2d2.367515577030239!3d48.85052157133101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67203d84b8295%3A0x33b4368a40547d54!2s61%20Rue%20de%20Lyon%2C%2075012%20Paris%2C%20France!5e0!3m2!1sen!2stn!4v1758130198578!5m2!1sen!2stn" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4031883118428!2d2.367515577030239!3d48.85052157133101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67203d84b8295%3A0x33b4368a40547d54!2s61%20Rue%20de%20Lyon%2C%2075012%20Paris%2C%20France!5e0!3m2!1sen!2stn!4v1758130198578!5m2!1sen!2stn" width="600" height="450" style="border:0;" allowfullscreen="" loading="eager" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
                 <div>
                     <div class="max-w-full mx-auto rounded-lg overflow-hidden">
                         <div class="px-6 py-4">
--- src/routes/services/telecom/+page.svelte
+++ src/routes/services/telecom/+page.svelte
@@ -1,36 +1,62 @@
 <script>
-    import fibreOptiqueLogo from '$lib/images/fibrelogo.webp'
+  const features = [
+    {
+      title: "Installation complète",
+      description: "Études, tirage, soudure et raccordement de câbles fibre optique conformes aux normes."
+    },
+    {
+      title: "Tous types de locaux",
+      description: "Interventions dans les immeubles, bureaux, commerces, sites industriels et bâtiments publics."
+    },
+    {
+      title: "Techniciens qualifiés",
+      description: "Experts en réseaux FTTH / FTTB / FTTO avec certifications reconnues."
+    },
+    {
+      title: "Maintenance & dépannage",
+      description: "Assistance rapide et fiable pour garantir la continuité de vos services."
+    }
+  ];
+
+  import opticalfiberimg from '$lib/images/opticalfiber.webp';
 </script>
 
+<section class="bg-gradient-to-r from-green-400 via-blue-500 to-indigo-600 text-white">
+  <div class="max-w-7xl mx-auto px-6 py-24 text-center">
+    <h1 class="text-4xl sm:text-5xl font-bold mb-6">TÉLÉCOM</h1>
+    <p class="text-lg sm:text-xl"><span class="font-bold">ECO STAF</span> Des solutions fibre optique rapides, sûres et durables pour tous vos espaces.</p>
 
-<section
-  class=" light relative py-[60px] md:pt-0 md:pb-[100px] z-[1] mx-auto my-10 md:my-24"
->
+  </div>
+</section>
 
-
-  <div class="container px-16 md:px-8 lg:px-28 w-full m-auto">
-    <div class="grid grid-cols-12 justify-between">
-      <div class="col-span-12 lg:col-span-5 flex flex-col justify-center ">
-        <h2 class="font-bold text-[32px] leading-none md:text-[45px] mb-6 mt-0 md:mt-12">Fibre Optique !</h2>
-        <p class="text-lg opacity-80 mb-12 lg:mb-0 font-semibold leading-relaxed">
-       <span class="font-bold"> Nos équipes accompagnent chaque étape du cycle de vie des réseaux d’accès TELECOM, FTTH et xDSL, toutes générations confondues.</span><br><br>
-Nous intervenons sur :<br>
-
-La réalisation d'infrastructures en génie civil et en aérien.<br>
-Le déploiement de réseaux fibre optique, cuivre et coaxiaux.<br>
-
-L’installation, l’upgrade, le swap et la mise en service d’équipements actifs.<br>
-Le raccordement des abonnés et les interventions SAV.<br>
-La maintenance préventive et curative des réseaux.
-        </p>
-      </div>
-      <div class="col-span-12 lg:col-span-5 lg:col-start-8 text-center justify-center items-center">
-        <img
-          src={fibreOptiqueLogo}
-          alt=""
-          class=" mx-auto md:py-20 rounded-md max-h-[700px]"
-        />
-      </div>
+<section id="services" class="py-20 ">
+  <div class="max-w-7xl mx-auto px-6">
+    
+    <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      {#each features as feature}
+        <div class=" p-6 rounded-lg shadow hover:shadow-lg transition">
+          <h3 class="text-xl font-semibold mb-2">{feature.title}</h3>
+          <p class="">{feature.description}</p>
+        </div>
+      {/each}
     </div>
   </div>
 </section>
+
+
+
+<!-- Image Section -->
+<section class="py-16 ">
+  <div class="max-w-7xl mx-auto px-6 flex justify-center">
+    <div class="w-full sm:w-3/4 lg:w-1/2">
+      <img 
+        src={opticalfiberimg} 
+        alt="Installation de borne électrique" 
+        class="w-full h-auto rounded-lg shadow-lg object-cover"
+      />
+    </div>
+  </div>
+</section>
+
+
+
